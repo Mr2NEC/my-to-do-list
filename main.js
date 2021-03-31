04077b46785246100c24bc906db96f12d6cdf1ee
git <help.vivat@gmail.com>
2021-03-31 14:22:56 +0300

    fix id

--- main.js
+++ main.js
@@ -1,20 +1,21 @@
 class TodoBody {
+    _ls = new Map(JSON.parse(localStorage.getItem('myToDos') || '[]'));
+    _todosList = new Map(this._ls);
+
     constructor(body) {
         this.body = body;
         body.onclick = this.onClick.bind(this);
-        this.ls = new Map(JSON.parse(localStorage.getItem("myToDos")) || []);
-        this.todosList = new Map(this.ls);
     }
 
     updateScope() {
-        const scope = document.querySelector(".to-do-list-info-scope");
-        scope.innerText = this.todosList.size;
+        const scope = document.querySelector('.to-do-list-info-scope');
+        scope.innerText = this._todosList.size;
     }
 
     updateActive() {
-        const active = document.querySelector(".to-do-list-info-active");
+        const active = document.querySelector('.to-do-list-info-active');
         let count = 0;
-        for (let todo of this.todosList.values()) {
+        for (let todo of this._todosList.values()) {
             if (!todo.done) {
                 count++;
             }
@@ -24,10 +25,10 @@ class TodoBody {
 
     updateSuccessful() {
         const successful = document.querySelector(
-            ".to-do-list-info-successful"
+            '.to-do-list-info-successful',
         );
         let count = 0;
-        for (let todo of this.todosList.values()) {
+        for (let todo of this._todosList.values()) {
             if (todo.done) {
                 count++;
             }
@@ -35,33 +36,25 @@ class TodoBody {
         successful.innerText = count;
     }
 
-    textDate(date) {
-        return date
-            .toLocaleDateString("en-GB", {
-                day: "numeric",
-                month: "short",
-                year: "numeric",
-            })
-            .split(" ")
-            .join(" ");
+    createId() {
+        return Number(new Date()) + '' + Math.floor(Math.random() * 100000);
     }
 
-    createId() {
-        let newid = () => {
-            return Math.floor((1 + Math.random()) * 0x10000)
-                .toString(16)
-                .substring(1);
-        };
-        return (
-            newid() +
-            newid() +
-            "-" +
-            newid() +
-            "-" +
-            newid() +
-            newid() +
-            newid()
-        );
+    updateHeders() {
+        this.updateScope();
+        this.updateActive();
+        this.updateSuccessful();
+    }
+
+    textDate(date) {
+        return date
+            .toLocaleDateString('en-GB', {
+                day: 'numeric',
+                month: 'short',
+                year: 'numeric',
+            })
+            .split(' ')
+            .join(' ');
     }
 
     getFormattedDate(date) {
@@ -69,130 +62,123 @@ class TodoBody {
     }
 
     templateTodo({ id, text, done, date }) {
-        const tmpl = document.querySelector("#tmpl");
+        const tmpl = document.querySelector('#tmpl');
         const clon = tmpl.content.cloneNode(true);
 
-        clon.querySelector(".to-do-list-content-item").id = id;
+        clon.querySelector('.to-do-list-content-item').id = id;
 
-        const checkbox = clon.querySelector("[data-action=updateChecked]");
+        const checkbox = clon.querySelector('[data-action=updateChecked]');
         checkbox.dataset.id = id;
         checkbox.checked = done;
 
-        const time = clon.querySelector("time");
-        time.setAttribute("datetime", this.getFormattedDate(date));
+        const time = clon.querySelector('time');
+        time.setAttribute('datetime', this.getFormattedDate(date));
         time.textContent = this.textDate(date);
 
-        const updateTodoOn = clon.querySelector("[data-action=updateTodoOn]");
+        const updateTodoOn = clon.querySelector('[data-action=updateTodoOn]');
         updateTodoOn.dataset.id = id;
-        if (done) updateTodoOn.classList.add("disabled");
+        if (done) updateTodoOn.classList.add('disabled');
 
-        clon.querySelector("[data-action=deleteTodo]").dataset.id = id;
+        clon.querySelector('[data-action=deleteTodo]').dataset.id = id;
 
-        const contentEditableDiv = clon.querySelector("[contentEditable]");
+        const contentEditableDiv = clon.querySelector('[contentEditable]');
         contentEditableDiv.dataset.id = id;
         contentEditableDiv.textContent = text;
-        if (done) contentEditableDiv.classList.add("checked");
+        if (done) contentEditableDiv.classList.add('checked');
 
         this.body.insertBefore(clon, this.body.firstElementChild);
     }
 
-    createTodo ( { id = this.createId(), text = "", done = false, date = new Date() } ) {
-        this.todosList.set(id, { text, done, date});
-        this.templateTodo( { id, text, done, date } );
-        this.updateScope();
-        this.updateActive();
+    createTodo({ id = this.createId(), text = '', done = false }) {
+        let date = new Date(Number(id.slice(0, 13)));
+        this._todosList.set(id, { text, done });
+        this.templateTodo({ id, text, done, date });
+        this.updateHeders();
     }
 
     deleteTodo({ id }) {
         const todoElem = document.getElementById(id);
         todoElem.remove();
-        this.todosList.delete(id);
-        this.updateLocalStorage({ type: "delete", id });
-        this.updateScope();
-        this.updateActive();
-        this.updateSuccessful();
+        this._todosList.delete(id);
+        this.updateLocalStorage({ type: 'delete', id });
     }
 
     updateTodoOn({ elem, id }) {
         const todoElem = document
             .getElementById(id)
-            .querySelector("[contentEditable]");
+            .querySelector('[contentEditable]');
         document
             .getElementById(id)
-            .querySelector("[data-action=updateChecked]").disabled = true;
-        elem.dataset.action = "updateTodoOff";
-        todoElem.setAttribute("contentEditable", "true");
+            .querySelector('[data-action=updateChecked]').disabled = true;
+        elem.dataset.action = 'updateTodoOff';
+        todoElem.setAttribute('contentEditable', 'true');
         todoElem.focus();
     }
 
     updateTodoOff({ elem, id }) {
         const todoElem = document
             .getElementById(id)
-            .querySelector("[contentEditable]");
-        if (todoElem.innerText !== "") {
-            todoElem.removeAttribute("oninput");
+            .querySelector('[contentEditable]');
+        if (todoElem.innerText !== '') {
+            todoElem.removeAttribute('oninput');
             document
                 .getElementById(id)
-                .querySelector("[data-action=updateChecked]").disabled = false;
-            elem.dataset.action = "updateTodoOn";
-            this.todosList.get(id).text = todoElem.innerText;
-            this.updateLocalStorage({ type: "update", id });
-            todoElem.setAttribute("contentEditable", "false");
+                .querySelector('[data-action=updateChecked]').disabled = false;
+            elem.dataset.action = 'updateTodoOn';
+            this._todosList.get(id).text = todoElem.innerText;
+            this.updateLocalStorage({ type: 'update', id });
+            todoElem.setAttribute('contentEditable', 'false');
         }
     }
 
     updateChecked({ elem, id }) {
         const todoElem = document
             .getElementById(id)
-            .querySelector("[contentEditable]");
+            .querySelector('[contentEditable]');
         elem.checked
-            ? todoElem.classList.add("checked")
-            : todoElem.classList.remove("checked");
+            ? todoElem.classList.add('checked')
+            : todoElem.classList.remove('checked');
 
         const updateTodoOn = document
             .getElementById(id)
-            .querySelector("[data-action=updateTodoOn]");
+            .querySelector('[data-action=updateTodoOn]');
         elem.checked
-            ? updateTodoOn.classList.add("disabled")
-            : updateTodoOn.classList.remove("disabled");
+            ? updateTodoOn.classList.add('disabled')
+            : updateTodoOn.classList.remove('disabled');
 
-        this.todosList.get(id).done = elem.checked;
-        this.updateLocalStorage({ type: "update", id });
-        this.updateActive();
-        this.updateSuccessful();
+        this._todosList.get(id).done = elem.checked;
+        this.updateLocalStorage({ type: 'update', id });
+        this.updateHeders();
     }
 
     toDoInit() {
-        if (this.todosList.size !== 0) {
-            for (let todo of this.todosList) {
+        if (this._todosList.size !== 0) {
+            for (let todo of this._todosList) {
                 this.createTodo({
                     id: todo[0],
                     text: todo[1].text,
                     done: todo[1].done,
-                    date: new Date(todo[1].date),
                 });
             }
         }
-        this.updateActive();
-        this.updateScope();
-        this.updateSuccessful();
+        this.updateHeders();
     }
 
-    updateLocalStorage ( { type, id } ) {
+    updateLocalStorage({ type, id }) {
         switch (type) {
-            case "delete":
-                this.ls.delete(id);
+            case 'delete':
+                this._ls.delete(id);
                 break;
-            case "update":
-                if (this.todosList.get(id).text !== "") {
-                    this.ls.set(id, this.todosList.get(id));
+            case 'update':
+                if (this._todosList.get(id).text !== '') {
+                    this._ls.set(id, this._todosList.get(id));
                 }
                 break;
 
             default:
                 break;
         }
-        localStorage.setItem("myToDos", JSON.stringify(Array.from(this.ls)));
+        localStorage.setItem('myToDos', JSON.stringify(Array.from(this._ls)));
     }
 
     onClick(event) {
@@ -204,7 +190,7 @@ class TodoBody {
     }
 }
 
-window.addEventListener("load", function () {
-    const TODOBODY = document.querySelector(".to-do-list-content-body");
-    new TodoBody( TODOBODY ).toDoInit();
+window.addEventListener('load', function () {
+    const TODOBODY = document.querySelector('.to-do-list-content-body');
+    new TodoBody(TODOBODY).toDoInit();
 });
